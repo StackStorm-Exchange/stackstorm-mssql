1d37b85924645280f926f6b0363228438ff6498d
Lindsay Hill <lindsay.k.hill@gmail.com>
2018-11-08 14:36:27 -0800

    add port kwarg

--- actions/mssql_runner.py
+++ actions/mssql_runner.py
@@ -1,6 +1,6 @@
-import _mssql
 import ast
 import sys
+import _mssql
 
 from lib.mssql_action import MSSQLAction
 from lib.results_processor import ResultsProcessor
@@ -16,7 +16,7 @@ class MSSQLRunner(MSSQLAction):
     # Pack actions map to a pair of methods: PyMSSQL (_mssql) driver and ResultsProcessor handler
     ACTION_MAPPING = {
         'execute_insert': ('execute_non_query', 'execute_insert')
-        # default: (action_name, action_name)
+                          # default: (action_name, action_name)
     }
 
     def __init__(self, config):
@@ -24,11 +24,11 @@ class MSSQLRunner(MSSQLAction):
         self.processor = ResultsProcessor(self.config)
 
     def run(self, action, query_string, params=None,
-            database=None, server=None, user=None, password=None):
+            database=None, server=None, user=None, password=None, port=None):
         try:
             # action corresponds to a pair of _mssql and ResultsProcessor methods
             driver_action, processor_action = self.ACTION_MAPPING.get(action, (action, action))
-            with self.connect(database, server, user, password) as cursor:
+            with self.connect(database, server, user, password, port) as cursor:
                 params = ast.literal_eval(params) if params else None
                 response = getattr(cursor, driver_action)(query_string, params)
                 return getattr(self.processor, processor_action)(response, cursor)
